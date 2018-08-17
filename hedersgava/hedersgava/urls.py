39ba7cbdc1129452465a80fd9d4e7c3b68853ea7
Linnovs <linnovs.liu@en-trak.com>
2018-08-17 20:05:08 +0800

    Add pylint ignore line inline config

--- hedersgava/hedersgava/urls.py
+++ hedersgava/hedersgava/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import url
 from echo import views
 
-urlpatterns = [
+urlpatterns = [  # pylint: disable=invalid-name
     url(r'^echo/$', views.echo),
     url('admin/', admin.site.urls),
 ]
